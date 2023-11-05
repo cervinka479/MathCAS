7d3b9ff9b4f602b8862cf0a2ceb4b686ae7dcffb
Laix974 <eduard.cervinka@outlook.com>
2023-11-05 14:48:21 +0100

    update

--- Tools/ANN/ANN.py
+++ Tools/ANN/ANN.py
@@ -1,24 +1,31 @@
-import torch
 import torch.nn as nn
-import torch.optim as optim
-import pandas as pd
-import numpy
-#import DataPrep.normalizeDataset as Data
-#import DataPrep.inverseNormalizeDataset as Data2
-import matplotlib.pyplot as plt
 
-def ANN(IO=[9,2],hiddenLayers=[12]):
+def nnArch(io=[9,1], hl=[12]):
     class NeuralNetwork(nn.Module):
-        def __init__(self, hidden_size):
+        def __init__(self, hl):
             super(NeuralNetwork, self).__init__()
-            self.fc1 = nn.Linear(IO[0], hidden_size)
-            self.relu = nn.ReLU()
-            self.fc2 = nn.Linear(hidden_size, IO[1])
+            self.layers = nn.ModuleList()
+            input_size = io[0]
+            
+            # Create hidden layers
+            for hidden_size in hl:
+                self.layers.append(nn.Linear(input_size, hidden_size))
+                self.layers.append(nn.ReLU())
+                input_size = hidden_size
+            
+            # Create output layer
+            self.layers.append(nn.Linear(input_size, io[1]))
+
+        def forward(self, x):
+            for layer in self.layers:
+                x = layer(x)
+            return x
 
     # Create the neural network instance
-    model = NeuralNetwork(hiddenLayers[0])
-
-    # Print the model architecture
+    model = NeuralNetwork(hl)
     print(model)
+    return model
 
-ANN(hiddenLayers=[16])
+
+
+nnArch(hl=[16,10,8])
