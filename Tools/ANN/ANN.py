b8ac3270f9f61d734d03a0700a6c863255637094
Laix974 <eduard.cervinka@outlook.com>
2023-11-23 23:44:39 +0100

    vizualization of losses

--- Tools/ANN/ANN.py
+++ Tools/ANN/ANN.py
@@ -27,13 +27,9 @@ def nnArch(io=[9,1], hl=[12]):
     model = NeuralNetwork(hl)
     return model
 
-def nnTrain(splitDataset=[["train_input"],["train_output"],["val_input"],["val_output"],["test_input"],["test_output"]], model=nnArch(),optimizer="adam",learningRate=0.01,criterion="mse", epochs=50, save=""):
+def nnTrainOld(splitDataset=[["train_input"],["train_output"],["val_input"],["val_output"],["test_input"],["test_output"]], model=nnArch(),optimizer="adam",learningRate=0.01,criterion="mse", epochs=50, save=""):
     import torch
     from torch.utils.data import DataLoader, TensorDataset
-
-    if splitDataset == [["train_input"],["train_output"],["val_input"],["val_output"],["test_input"],["test_output"]]:
-        print("E: Invalid dataset")
-        return
 
     # Unpack your dataset
     train_input, train_output, val_input, val_output, test_input, test_output = splitDataset
@@ -62,6 +58,8 @@ def nnTrain(splitDataset=[["train_input"],["train_output"],["val_input"],["val_o
 
     # Training loop
     for epoch in range(epochs):
+        train_loss = 0
+        val_loss = 0
         for inputs, targets in train_loader:
             # Forward pass
             outputs = model(inputs)
@@ -87,14 +85,123 @@ def nnTrain(splitDataset=[["train_input"],["train_output"],["val_input"],["val_o
         torch.save(model.state_dict(), save+".pth")
         print("saved model: "+save+".pth")
 
-def nnPredict(model="saved_model_name"):
-    if model == "saved_model_name":
-        print("E: Invalid model")
-        return
+def nnTrainVal(splitDataset=[["train_input"],["train_output"],["val_input"],["val_output"],["test_input"],["test_output"]], model=nnArch(),optimizer="adam",learningRate=0.01,criterion="mse", epochs=50, save=""):
+    import torch
+    from torch.utils.data import DataLoader, TensorDataset
+
+    # Unpack your dataset
+    train_input, train_output, val_input, val_output, test_input, test_output = splitDataset
+
+    # Convert your dataset to PyTorch tensors
+    train_data = TensorDataset(torch.tensor(train_input, dtype=torch.float32), torch.tensor(train_output, dtype=torch.float32))
+    val_data = TensorDataset(torch.tensor(val_input, dtype=torch.float32), torch.tensor(val_output, dtype=torch.float32))
+    test_data = TensorDataset(torch.tensor(test_input, dtype=torch.float32), torch.tensor(test_output, dtype=torch.float32))
     
-    print("it is working")
+    # Create a DataLoader for your dataset
+    train_loader = DataLoader(train_data, batch_size=5)
+    val_loader = DataLoader(val_data, batch_size=5)
+    test_loader = DataLoader(test_data, batch_size=5)
+
+    print(model)
+
+    match optimizer:
+        case "sgd":
+            optimizer = torch.optim.SGD(model.parameters(), learningRate)
+        case "adam":
+            optimizer = torch.optim.Adam(model.parameters(), learningRate)
+
+    match criterion:
+        case "mse":
+            criterion=torch.nn.MSELoss()
+
+    train_losses = []
+    val_losses = []
+
+    # Training loop
+    for epoch in range(epochs):
+        train_loss = 0
+        val_loss = 0
+        for inputs, targets in train_loader:
+            # Forward pass
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+        
+        train_loss /= len(train_loader)
+
+        # Validation at the end of the epoch
+        model.eval()  # Set the model to evaluation mode
+        with torch.no_grad():
+            for inputs, targets in val_loader:
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                val_loss += loss.item()
+            val_loss /= len(val_loader)  # Average validation loss
+
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+        print(f'Epoch {epoch+1}/{epochs}, Training Loss: {loss.item()}, Validation Loss: {val_loss}')
+
+        model.train()  # Set the model back to training mode
+
+    # Evaluation
+    with torch.no_grad():
+        for inputs, targets in test_loader:
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            print(f'Final Loss: {loss.item()}')
+
+    # Save trained model
+    if save != "":
+        torch.save(model.state_dict(), save+".pth")
+        print("saved model: "+save+".pth")
+
+    return train_losses, val_losses
+
+def nnPredict(loadModel, inputDataset, model=nnArch()):
+    import torch
+    from torch.utils.data import DataLoader, TensorDataset
+    
+    # Load the saved model
+    model.load_state_dict(torch.load(loadModel))  # Load the saved parameters
+    model.eval()  # Set the model to evaluation mode
+
+    # Convert your input data to a PyTorch tensor
+    inputDataset = torch.tensor(inputDataset, dtype=torch.float32)
+
+    # Use the model to make predictions
+    with torch.no_grad():
+        predictions = model(inputDataset)
+
+    return predictions
 
 
-#nnTrain(save="testModel", splitDataset=DataPrep.split(*DataPrep.extract("test.csv")),model=nnArch(io=[2,1]), epochs=200)
+#nnTrain(save="testModelFX", splitDataset=DataPrep.split(*DataPrep.extract("datasetFX.csv")),model=nnArch(io=[2,1]), epochs=200, learningRate=0.001)
 
-nnPredict()
+#print(nnPredict(loadModel="testModelFX.pth", inputDataset=DataPrep.extract("datasetPredict.csv",i=[1,2],o=[1,1])[0],model=nnArch(io=[2,1])))
+
+import matplotlib.pyplot as plt
+
+# Train your model and get the losses
+train_losses, val_losses = nnTrainVal(splitDataset=DataPrep.split(*DataPrep.extract("datasetSum.csv")),model=nnArch(io=[2,1]), epochs=200, learningRate=0.001)
+
+# Create a figure
+plt.figure()
+
+# Plot the training loss
+plt.plot(train_losses, label='Training Loss')
+
+# Plot the validation loss
+plt.plot(val_losses, label='Validation Loss')
+
+# Add a legend
+plt.legend()
+
+# Show the plot
+plt.show()
