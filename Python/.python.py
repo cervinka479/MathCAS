5952b7cdb0b265af216525b1fcfebfc271c6fe65
Laix974 <eduard.cervinka@outlook.com>
2023-12-07 23:07:24 +0100

    added Training CLI

--- Python/.python.py
+++
@@ -1,5 +0,0 @@
-lst = [1, 2, 3]
-for i in lst:
-    print(i)
-    if i < 5:
-        lst.append(i+1)
--- Tools/ANN/ANN.py
+++ Tools/ANN/ANN.py
@@ -27,9 +27,10 @@ def nnArch(io=[9,1], hl=[12]):
     model = NeuralNetwork(hl)
     return model
 
-def nnTrain(splitDataset=[["train_input"],["train_output"],["val_input"],["val_output"],["test_input"],["test_output"]], model=nnArch(),optimizer="adam",learningRate=0.01,criterion="mse",batch_size=4, epochs=50, save="", visualize=True):
+def nnTrain(splitDataset=[["train_input"],["train_output"],["val_input"],["val_output"],["test_input"],["test_output"]], model=nnArch(),optimizer="adam",learningRate=0.01,criterion="mse",batch_size=4, epochs=50, save="test", visualize=True, cli=True):
     import torch
     from torch.utils.data import DataLoader, TensorDataset
+    import copy
 
     # Unpack your dataset
     train_input, train_output, val_input, val_output, test_input, test_output = splitDataset
@@ -58,59 +59,88 @@ def nnTrain(splitDataset=[["train_input"],["train_output"],["val_input"],["val_o
 
     train_losses = []
     val_losses = []
+    models = []
     minimal_val_loss = "x"
+    saveNum = 1
 
     # Training loop
-    for epoch in range(epochs):
-        train_loss = 0
-        val_loss = 0
-        for inputs, targets in train_loader:
-            # Forward pass
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-        
-        train_loss /= len(train_loader)
-
-        # Validation at the end of the epoch
-        model.eval()  # Set the model to evaluation mode
-        with torch.no_grad():
-            for inputs, targets in val_loader:
+    while True:
+        for epoch in range(epochs):
+            train_loss = 0
+            val_loss = 0
+            for inputs, targets in train_loader:
+                # Forward pass
                 outputs = model(inputs)
                 loss = criterion(outputs, targets)
-                val_loss += loss.item()
-            val_loss /= len(val_loader)  # Average validation loss
-
-        train_losses.append(train_loss)
-        val_losses.append(val_loss)
-
-        print(f'Epoch {epoch+1}/{epochs}, Training Loss: {loss.item()}, Validation Loss: {val_loss}')
-
-        model.train()  # Set the model back to training mode
-
-        # Save trained model
-        if save != "":
+            
+                # Backward pass and optimization
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.item()
+            
+            train_loss /= len(train_loader)
+            
+            # Validation at the end of the epoch
+            model.eval()  # Set the model to evaluation mode
+            with torch.no_grad():
+                for inputs, targets in val_loader:
+                    outputs = model(inputs)
+                    loss = criterion(outputs, targets)
+                    val_loss += loss.item()
+                val_loss /= len(val_loader)  # Average validation loss
+            
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            
+            print(f'Epoch {epoch+1}/{epochs}, Training Loss: {loss.item()}, Validation Loss: {val_loss}')
+            
+            model.train()  # Set the model back to training mode
+            
+            models.append(copy.deepcopy(model.state_dict()))
+            
+            # Save trained model
             if minimal_val_loss == "x":
                 minimal_val_loss = val_loss
             elif val_loss < minimal_val_loss:
                 bestModel = model.state_dict()
                 minimal_val_loss = val_loss
+        
+        print("minimal validation loss: "+str(minimal_val_loss))
+        
+        # Visualize the training process
+        if visualize == True:
+            nnVisualize(train_losses, val_losses)
+        
+        # Interactive Training CLI
+        if cli == True:
+            while True:
+                try:
+                    answer = input("\nTraining CLI:\nexit - to break the training loop\nshow - to show the visualization\nnext () - (number) to train further for number of epochs\nsave () - (number) to save the model from that epoch, (min) to save the model with the lowest validation loss\n\n")
+                    if answer[0:4] == "exit":
+                        cli = False
+                        break
+                    elif answer[0:4] == "show":
+                        nnVisualize(train_losses, val_losses)
+                    elif answer[0:4] == "next":
+                        epochs = int(answer[5:])
+                        break
+                    elif answer[0:4] == "save":
+                        if answer[5:] == "min":
+                            savePath = save+str(saveNum)+"_VL{"+str("{:.3e}".format(minimal_val_loss))+"}.pth"
+                            torch.save(bestModel, savePath)
+                        else:
+                            savePath = save+str(saveNum)+"_VL{"+str("{:.3e}".format(val_losses[int(answer[5:])]))+"}.pth"
+                            torch.save(models[int(answer[5:])], savePath)
+                        saveNum = saveNum + 1
+                        print("saved model: "+savePath)
+                except Exception:
+                    print("E: Input is in wrong format")
+        
+        if cli == False:
+            break
 
-    print("minimal validation loss: "+str(minimal_val_loss))
-    
-    if save != "":
-        savePath = save+"_VL{"+str("{:.3e}".format(minimal_val_loss))+"}.pth"
-        torch.save(bestModel, savePath)
-        print("saved model: "+savePath)
-    
-    # Visualize the training process
-    if visualize == True:
-        nnVisualize(train_losses, val_losses)
+
 
     #return train_losses, val_losses
     return minimal_val_loss
@@ -139,8 +169,8 @@ def nnVisualize(train_losses,val_losses):
     plt.figure()
 
     # Plot the training and validation losses for the model
-    plt.plot(train_losses, label='Training Loss')
-    plt.plot(val_losses, label='Validation Loss')
+    plt.plot(train_losses, '-o', label='Training Loss')
+    plt.plot(val_losses, '-o', label='Validation Loss')
 
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
@@ -149,7 +179,6 @@ def nnVisualize(train_losses,val_losses):
     # Show the plot
     plt.show()
 
-# Loss realtion to dataset scale
 def valLossComparasion():
     import matplotlib.pyplot as plt
     import numpy as np
@@ -174,8 +203,8 @@ def valLossComparasion():
     plt.show()
 
 
-print(nnPredict(loadModel="lrTest4_VL{1.970e-08}.pth", inputDataset=DataPrep.extract("dPredict.csv",i=[1,2],o=[1,1])[0],model=nnArch(io=[2,1],hl=[16])))
+#print(nnPredict(loadModel="test2_VL{3.271e-01}.pth", inputDataset=DataPrep.extract("dPredict.csv",i=[1,2],o=[1,1])[0],model=nnArch(io=[2,1],hl=[16])))
 
 #valLossComparasion()
 
-#nnTrain(save="lrTest4", splitDataset=DataPrep.split(*DataPrep.extract("dXY-5_1000.csv",limit=800)),model=nnArch(io=[2,1], hl=[16]), epochs=1200, learningRate=0.0001)
+nnTrain(cli=True, splitDataset=DataPrep.split(*DataPrep.extract("dXY-5_1000.csv",limit=200)),model=nnArch(io=[2,1], hl=[16]), epochs=10, learningRate=0.01)
