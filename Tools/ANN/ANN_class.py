440e5939d4f0bac2c587662d6365734aced58458
Laix974 <eduard.cervinka@outlook.com>
2024-01-13 13:17:19 +0100

    updt

--- Tools/ANN/ANN_class.py
+++ Tools/ANN/ANN_class.py
@@ -223,17 +223,17 @@ def valLossComparasion():
 
 modelArchitecture = nnArch(io=[3,1], hl=[32,16])
 
-'''# Trainig section
-extractedData = DataPrep.extract(path="dOmegaRES10k.csv",i=[1,3],o=[4,4],limit=8000)
+# Trainig section
+extractedData = DataPrep.extract(path="dOmegaRES10k.csv",i=[1,3],o=[4,4],limit=800)
 froScaledData = DataPrep.scale(extractedData[0],extractedData[1],method="fro")
 
 nnTrain(save="classifierTest",splitDataset=DataPrep.split(*extractedData),model=modelArchitecture, epochs=100, learningRate=0.001, batch_size=8)
 #nnTrain(save="classifierTest",splitDataset=DataPrep.split(*froScaledData),model=modelArchitecture, epochs=50, learningRate=0.001, batch_size=8)
-'''
 
-# Predicting section
+
+'''# Predicting section
 extractedData = DataPrep.extract(path="dTest.csv",i=[1,3],o=[4,4])
 froScaledData = DataPrep.scale(extractedData[0],extractedData[1],method="fro")
 
 print(nnPredict(loadModel="classifierTest1_VL{-2.491e+00}.pth", testDataset=extractedData,model=modelArchitecture)[0])
-#print(DataPrep.inverseScale(extractedData[0],nnPredict(loadModel="8kTestModel1_VL{2.297e-06}.pth", testDataset=froScaledData,model=modelArchitecture)[0],method="fro"))
+#print(DataPrep.inverseScale(extractedData[0],nnPredict(loadModel="8kTestModel1_VL{2.297e-06}.pth", testDataset=froScaledData,model=modelArchitecture)[0],method="fro"))'''
